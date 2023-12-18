6c0266e00f6796f68ec028084b0505eecd6671d2
LukePadawalker <padurariu.luca@gmail.com>
2023-12-18 15:35:39 +0100

    created the flowchart

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 console.log('JS OK');
 
+/*
+
+ - crea una variabile km
+ - chiedi all'utente quanti chilometri vuole percorrere
+ - crea una variabile eta
+ - chiedi all'utente l'eta
+ - crea una variabile prezzochilometro (costante)
+ - crea una variabile sconto20 (costante)
+ - crea una variabile sconto40 (costante)
+ - crea una variabile prezzo-finale
+ - crea una condizione:
+     se l'eta è minore di 18 anni
+         allora calcola il prezzo finale, applica lo sconto del 20% (sconto20)
+     altrimenti se l'eta è maggiore di 65 anni
+         allora calcola il prezzo finale, applica lo sconto del 40% (sconto40)
+    altrimenti
+        calcola il prezzo finale standard 
+ - e mostra all'utente il prezzo finale
+        
+
+     
+
+
+
+*/
+
+
